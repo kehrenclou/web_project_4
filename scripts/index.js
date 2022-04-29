78d2964786fccb7b5a755c8bc4809abaff75c9c0
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2022-04-29 15:03:55 -0700

    add Card Template

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,43 @@
-///////////////
-//Declarations
-///////////////
+/* -------------------------------------------------------------------------- */
+/*                                Declarations                                */
+/* -------------------------------------------------------------------------- */
 
-//Wrappers
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+/* -------------------------------- wrappers -------------------------------- */
 const editProfileModal = document.querySelector("#modal-edit-profile");
 const editProfileForm = document.querySelector("#modal-form-edit-profile");
+const placesList = document.querySelector(".cards__list");
 
-//Form data
+/* -------------------------------- Form data ------------------------------- */
 const profileNameInputValue = document.querySelector('[name="input-name"]');
 const profileAboutInputValue = document.querySelector('[name="input-about"]');
 
-//Buttons and other DOM elements
+/* --------------------- Buttons and other DOM elements --------------------- */
 const editProfileOpenButton = document.querySelector(
   "#edit-profile-open-button"
 );
@@ -18,11 +45,17 @@ const editProfileCloseButton = document.querySelector("#modal-close-button");
 
 const profileName = document.querySelector("#profile-name");
 const profileAbout = document.querySelector("#profile-about");
-//Wrappers
 
-/////////////
-//Functions
-////////////
+/* -------------------------------- Templates ------------------------------- */
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".cards__item");
+
+/* -------------------------------------------------------------------------- */
+/*                                  Functions                                 */
+/* -------------------------------------------------------------------------- */
+
+/* ------------------------------ profile Edit ------------------------------ */
 function openEditProfileModal() {
   editProfileModal.classList.add("modal_open");
 }
@@ -40,9 +73,27 @@ function populateProfileFromFormInputs() {
   profileAbout.textContent = profileAboutInputValue.value;
 }
 
-/////////////////
-//Event Handlers
-////////////////
+/* ------------------------------ card Template ----------------------------- */
+function createCard(card) {
+  //declarations - card template set globally
+  //clone template
+  const cardElement = cardTemplate.cloneNode(true);
+  //assign card elements
+  cardElement.querySelector(".cards__image").src = card.link;
+  cardElement.querySelector("#card-image").alt = card.name;
+  cardElement.querySelector(".cards__text").textContent = card.name;
+  //return card to DOM
+  return cardElement;
+
+  //add event listener
+}
+
+function renderCard(card) {
+  placesList.append(card);
+}
+/* -------------------------------------------------------------------------- */
+/*                               Event Listeners                              */
+/* -------------------------------------------------------------------------- */
 
 editProfileOpenButton.addEventListener(
   "click",
@@ -76,3 +127,11 @@ editProfileForm.addEventListener("submit", closeEditProfileModal);
 //     heartButton.classList.toggle("cards__button-like_active");
 //   });
 // });
+
+initialCards.forEach(function (card) {
+  //append to the list
+  const newCard = createCard(card);
+  renderCard(newCard);
+  // placesList.append(newCard);
+  //loop
+});
